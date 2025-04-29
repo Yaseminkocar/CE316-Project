ce958c58244075d57541349ce229c415f2968977
EmirayDurmaz <emiray09@hotmail.com>
2025-04-29 16:10:48 +0300

    Student class created

---
+++ src/main/java/com/ieuapp/Student.java
@@ -0,0 +1,63 @@
+package com.ieuapp;
+
+public class Student {
+    private String id;
+    private String output;
+
+    private boolean result;
+
+    private boolean isCompiled;
+    private boolean isRan;
+
+    public Student(String id, boolean result) {
+        this.id = id;
+        this.result = result;
+    }
+
+    public Student(){
+
+    }
+
+
+    public boolean isCompiled() {
+        return isCompiled;
+    }
+
+    public void setCompiled(boolean compiled) {
+        isCompiled = compiled;
+    }
+
+    public boolean isRan() {
+        return isRan;
+    }
+
+    public void setRan(boolean ran) {
+        isRan = ran;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getOutput() {
+        return output;
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
+    }
+
+    public boolean getResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+}
+
